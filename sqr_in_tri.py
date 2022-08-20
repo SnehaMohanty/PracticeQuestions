e287c357a8203e18b7d24eff5991293830b6c183
Sneha Mohanty <SnehaMohanty@users.noreply.github.com>
2022-08-20 20:03:40 +0200

    geeksforgeeks q2

---
+++ sqr_in_tri.py
@@ -0,0 +1,14 @@
+import math
+
+side_1 = 8
+side_2 = 8
+
+side_3 = math.sqrt(side_1^2 + side_2^2)
+
+area_triangle = 0.5*side_1*side_2
+
+area_sqr = 2*2
+
+num_of_sqr = int(area_triangle/area_sqr)
+
+print(num_of_sqr)
